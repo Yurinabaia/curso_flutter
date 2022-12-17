a72b99ae420fb1714bc56dbf6d687d4bd853ecb0
nabaia <yurinabaiaduarte@gmail.com>
2022-12-17 18:19:50 -0300

    Alteracao da autenticacao

--- lib/domain/usercases/autentication.dart
+++ lib/domain/usercases/autentication.dart
@@ -3,3 +3,10 @@ import '../entities/account_entity.dart';
 abstract class Authentication {
   Future<AccountEntity> auth({required String email, required String password});
 }
+
+class AuthenticationParams {
+  final String email;
+  final String password;
+
+  AuthenticationParams({required this.email, required this.password});
+}
