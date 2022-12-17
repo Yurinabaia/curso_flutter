0ec12673909cefd05f088558e763b9bddcb390a6
nabaia <yurinabaiaduarte@gmail.com>
2022-12-17 18:01:39 -0300

    Criado primeiro teste com rota api

---
+++ test/data/usecases/remote_autentication_test.dart
@@ -0,0 +1,34 @@
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+class RemoteAuthentication {
+  final HttpClient httpClient;
+  final String url;
+
+  RemoteAuthentication({required this.httpClient, required this.url});
+
+  auth() async {
+    await httpClient.request(url: url);
+  }
+}
+
+abstract class HttpClient {
+  request({required String url});
+}
+
+//Mockar classe concreta para classe abstrata
+class HttpClientSpy extends Mock implements HttpClient {}
+
+void main() {
+  test('Should call HttpClient with current URL', () async {
+    // arrange
+    final httpClient = HttpClientSpy();
+    final url = faker.internet.httpUrl();
+    final aut = RemoteAuthentication(httpClient: httpClient, url: url);
+    // act
+    await aut.auth();
+    // assert
+    verify(httpClient.request(url: url));
+  });
+}
