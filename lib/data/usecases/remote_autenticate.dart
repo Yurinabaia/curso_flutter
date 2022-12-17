f6c9a272280f9f0e316dbedd9245284d5bc92746
nabaia <yurinabaiaduarte@gmail.com>
2022-12-17 18:50:52 -0300

    Refatorando o post da API

--- lib/data/usecases/remote_autenticate.dart
+++ lib/data/usecases/remote_autenticate.dart
@@ -1,3 +1,7 @@
+import 'dart:html';
+
+import 'package:curso_flutter/domain/entities/account_entity.dart';
+
 import '../../domain/usercases/usercases.dart';
 
 import '../http/http.dart';
@@ -9,7 +13,23 @@ class RemoteAuthentication {
   RemoteAuthentication({required this.httpClient, required this.url});
 
   auth(AuthenticationParams authenticationParams) async {
-    await httpClient.request(
-        url: url, method: 'post', body: authenticationParams.toJson());
+    final body =
+        RemoteAuthenticationParams.fromDomain(authenticationParams).toJson();
+    await httpClient.request(url: url, method: 'post', body: body);
   }
+}
+
+class RemoteAuthenticationParams {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationParams({required this.email, required this.password});
+
+  factory RemoteAuthenticationParams.fromDomain(
+          AuthenticationParams authenticationParams) =>
+      RemoteAuthenticationParams(
+          email: authenticationParams.email,
+          password: authenticationParams.password);
+
+  Map toJson() => {'email': email, 'password': password};
 }
