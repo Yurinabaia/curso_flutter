addc18cfc07cf83062137926a5e5777c09b2133d
nabaia <yurinabaiaduarte@gmail.com>
2022-12-17 18:30:41 -0300

    Criado teste com body na requisicao

--- test/data/usecases/remote_autentication_test.dart
+++ test/data/usecases/remote_autentication_test.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: prefer_typing_uninitialized_variables
-
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
+
+import 'package:curso_flutter/domain/usercases/usercases.dart';
 
 class RemoteAuthentication {
   final HttpClient httpClient;
@@ -10,13 +11,17 @@ class RemoteAuthentication {
 
   RemoteAuthentication({required this.httpClient, required this.url});
 
-  auth() async {
-    await httpClient.request(url: url, method: 'post');
+  auth(AuthenticationParams authenticationParams) async {
+    final body = {
+      'email': authenticationParams.email,
+      'password': authenticationParams.password,
+    };
+    await httpClient.request(url: url, method: 'post', body: body);
   }
 }
 
 abstract class HttpClient {
-  request({required String url, required String method});
+  request({required String url, required String method, Map body});
 }
 
 //Mockar classe concreta para classe abstrata
@@ -29,16 +34,27 @@ void main() {
 
   //Separando Arange Act Assert
   setUp(() {
-    // arrange
+    // Arrange global
     httpClient = HttpClientSpy();
     url = faker.internet.httpUrl();
     aut = RemoteAuthentication(httpClient: httpClient, url: url);
   });
 
   test('Should call HttpClient with current values', () async {
+    //Arrange
+    final email = faker.internet.email();
+    final password = faker.internet.password();
+
     // act
-    await aut.auth();
+    await aut.auth(AuthenticationParams(email: email, password: password));
     // assert
-    verify(httpClient.request(url: url, method: 'post'));
+    verify(httpClient.request(
+      url: url,
+      method: 'post',
+      body: {
+        'email': email,
+        'password': password,
+      },
+    ));
   });
 }
