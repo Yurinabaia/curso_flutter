2197b4cb0f5045415f4460173752018780826d6e
nabaia <yurinabaiaduarte@gmail.com>
2022-12-20 23:38:39 -0300

    Criado regra de teste de status 200

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:curso_flutter/data/http/http.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
@@ -9,46 +10,55 @@ class ClientSpy extends Mock implements Client {
   @override
   Future<Response> post(Uri url,
       {Map<String, String>? headers, body, Encoding? encoding}) async {
-    return Response('any', 200);
+    return Response('{"any_key": "any_value"}', 200);
   }
 
   @override
   Future<Response> put(Uri url,
       {Map<String, String>? headers, body, Encoding? encoding}) async {
-    return Response('any', 200);
+    return Response('{"any_key": "any_value"}', 200);
   }
 
   @override
   Future<Response> patch(Uri url,
       {Map<String, String>? headers, body, Encoding? encoding}) async {
-    return Response('any', 200);
+    return Response('{"any_key": "any_value"}', 200);
   }
 
   @override
   Future<Response> delete(Uri url,
       {Map<String, String>? headers, body, Encoding? encoding}) async {
-    return Response('any', 200);
+    return Response('{"any_key": "any_value"}', 200);
   }
 
   @override
   Future<Response> get(Uri url,
       {Map<String, String>? headers, body, Encoding? encoding}) async {
-    return Response('any', 200);
+    return Response('{"any_key": "any_value"}', 200);
   }
 }
 
-class HttpAdapter {
+class HttpAdapter implements HttpClient {
   final Client client;
-  Future<void> request(
+  @override
+  Future<Map> request(
       {required String url, required String method, Map? body}) async {
-    await client.post(
-      Uri.http(url.split("//")[1], ''),
-      headers: {
-        'content-type': 'application/json',
-        'accept': 'application/json',
-      },
-      body: body != null ? jsonEncode(body) : null,
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+    };
+    final bodyFormat = body != null ? jsonEncode(body) : null;
+
+    final response = await client.post(
+      Uri.http(url, ''),
+      headers: headers,
+      body: bodyFormat,
     );
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return response.body.isEmpty ? {} : jsonDecode(response.body);
+    } else {
+      throw HttpError.serverError;
+    }
   }
 
   HttpAdapter(this.client);
@@ -62,7 +72,7 @@ void main() {
     // arrange
     client = ClientSpy();
     sut = HttpAdapter(client);
-    url = faker.internet.httpUrl();
+    url = faker.internet.httpUrl().split("//")[1];
   });
   group('post', () {
     test('Should call post with correct values', () async {
@@ -70,7 +80,27 @@ void main() {
       await sut
           .request(url: url, method: 'post', body: {'any_key': 'any_value'});
       // assert
-      expect(url, url);
+      when(client.post(Uri.http(url, ''),
+          headers: anyNamed('headers'), body: null));
+    });
+
+    test('Should call post with correct values with body', () async {
+      // act
+      await sut
+          .request(url: url, method: 'post', body: {'any_key': 'any_value'});
+      // assert
+      when(client.post(Uri.http(url, ''),
+          headers: anyNamed('headers'), body: anyNamed('body')));
+    });
+
+    test('Should call return data', () async {
+      // arrange
+      final Map map = {'any_key': 'any_value'};
+      // act
+      var response = await sut
+          .request(url: url, method: 'post', body: {'any_key': 'any_value'});
+      // assert
+      expect(response, map);
     });
   });
 }
