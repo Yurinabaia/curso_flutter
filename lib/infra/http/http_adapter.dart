bdfb2670d9093466b249ca5a36f0fc938e8d31b5
nabaia <yurinabaiaduarte@gmail.com>
2022-12-28 21:54:45 -0300

    Teste de erros na API post

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -7,7 +7,7 @@ import 'package:curso_flutter/data/http/http_error.dart';
 class HttpAdapter implements HttpClient {
   final Client client;
   @override
-  Future<Map> request(
+  Future<Map?> request(
       {required String url, required String method, Map? body}) async {
     final headers = {
       'content-type': 'application/json',
@@ -23,9 +23,19 @@ class HttpAdapter implements HttpClient {
     return _handleResponse(response);
   }
 
-  Map _handleResponse(Response response) {
+  Map? _handleResponse(Response response) {
     if (response.statusCode == 200 || response.statusCode == 201) {
       return response.body.isEmpty ? {} : jsonDecode(response.body);
+    } else if (response.statusCode == 204) {
+      return null;
+    } else if (response.statusCode == 400) {
+      throw HttpError.badRequest;
+    } else if (response.statusCode == 401) {
+      throw HttpError.unauthorized;
+    } else if (response.statusCode == 403) {
+      throw HttpError.forbidden;
+    } else if (response.statusCode == 404) {
+      throw HttpError.notFound;
     } else {
       throw HttpError.serverError;
     }
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,15 +1,29 @@
 import 'dart:convert';
 
-import 'package:curso_flutter/infra/http/http.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 
+import 'package:curso_flutter/data/http/http.dart';
+import 'package:curso_flutter/infra/http/http.dart';
+
 class ClientSpy extends Mock implements Client {
   @override
   Future<Response> post(Uri url,
       {Map<String, String>? headers, body, Encoding? encoding}) async {
+    var bodyResponse = jsonEncode(body);
+    if (bodyResponse.contains("badRequest")) {
+      return Response('{"any_key": "any_value"}', 400);
+    } else if (bodyResponse.contains("unauthorized")) {
+      return Response('{"any_key": "any_value"}', 401);
+    } else if (bodyResponse.contains("forbidden")) {
+      return Response('{"any_key": "any_value"}', 403);
+    } else if (bodyResponse.contains("notFound")) {
+      return Response('{"any_key": "any_value"}', 404);
+    } else if (bodyResponse.contains("serverError")) {
+      return Response('{"any_key": "any_value"}', 500);
+    }
     return Response('{"any_key": "any_value"}', 200);
   }
 
@@ -75,6 +89,45 @@ void main() {
           .request(url: url, method: 'post', body: {'any_key': 'any_value'});
       // assert
       expect(response, map);
+    });
+
+    test('Should call return erro 400', () async {
+      // act
+      final future = sut
+          .request(url: url, method: 'post', body: {'any_key': 'badRequest'});
+      // assert
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should call return erro 401', () async {
+      // act
+      final future = sut
+          .request(url: url, method: 'post', body: {'any_key': 'unauthorized'});
+      // assert
+      expect(future, throwsA(HttpError.unauthorized));
+    });
+
+    test('Should call return erro 403', () async {
+      // act
+      final future =
+          sut.request(url: url, method: 'post', body: {'any_key': 'forbidden'});
+      // assert
+      expect(future, throwsA(HttpError.forbidden));
+    });
+    test('Should call return erro 404', () async {
+      // act
+      final future =
+          sut.request(url: url, method: 'post', body: {'any_key': 'notFound'});
+      // assert
+      expect(future, throwsA(HttpError.notFound));
+    });
+
+    test('Should call return erro 500', () async {
+      // act
+      final future = sut
+          .request(url: url, method: 'post', body: {'any_key': 'serverError'});
+      // assert
+      expect(future, throwsA(HttpError.serverError));
     });
   });
 }
