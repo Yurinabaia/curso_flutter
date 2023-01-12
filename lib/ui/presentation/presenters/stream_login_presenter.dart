56d4c0a33f58fc6084c9992790781bc8cb28ad18
nabaia <yurinabaiaduarte@gmail.com>
2023-01-11 22:02:53 -0300

    Criando o restante do teste do formulario

--- lib/ui/presentation/presenters/stream_login_presenter.dart
+++ lib/ui/presentation/presenters/stream_login_presenter.dart
@@ -3,25 +3,50 @@ import 'dart:async';
 import '../protocols/protocols.dart';
 
 class LoginState {
+  String? email;
+  String? password;
   String? emailError;
   String? passwordError;
   String? mainError;
   String? navigateTo;
   bool? isLoading;
+  bool? get isFormValid =>
+      emailError == null &&
+      passwordError == null &&
+      email != null &&
+      password != null;
 }
 
 class StreamLoginPresenter {
   final Validation validation;
   //broadcast permite que mais de um listener possa ouvir o mesmo stream
   final _controller = StreamController<LoginState>.broadcast();
+  //Distinc deixa apenas um valor passar e não dois iguais.
   Stream<String?> get emailErrorStream =>
       _controller.stream.map((state) => state.emailError).distinct();
+
+  Stream<String?> get passwordErroStream =>
+      _controller.stream.map((state) => state.passwordError).distinct();
+
+  Stream<bool> get isFormValidStream =>
+      _controller.stream.map((state) => state.isFormValid == true).distinct();
 
   final _state = LoginState();
 
   StreamLoginPresenter({required this.validation});
+
+  void update() => _controller.add(_state);
+
   void validateEmail(String email) {
+    _state.email = email;
     _state.emailError = validation.validate(field: 'email', value: email);
-    _controller.add(_state);
+    update();
+  }
+
+  void validatePassword(String password) {
+    _state.password = password;
+    _state.passwordError =
+        validation.validate(field: 'password', value: password);
+    update();
   }
 }
--- test/presentatiom/presenters/stream_login_presenter_test.dart
+++ test/presentatiom/presenters/stream_login_presenter_test.dart
@@ -10,7 +10,8 @@ class ValidationSpy extends Mock implements Validation {}
 void main() {
   ValidationSpy validation = ValidationSpy();
   StreamLoginPresenter sut = StreamLoginPresenter(validation: validation);
-  String email = faker.internet.email();
+  String email = "";
+  String password = "";
 
   PostExpectation mockValidationCall(String? field) => when(validation.validate(
       field: field ?? anyNamed('field'), value: anyNamed('value')));
@@ -23,6 +24,7 @@ void main() {
     validation = ValidationSpy();
     sut = StreamLoginPresenter(validation: validation);
     email = faker.internet.email();
+    password = faker.internet.password();
     mockValidation();
   });
 
@@ -33,8 +35,87 @@ void main() {
 
   test('Should emit email error if validation fails', () {
     mockValidation(value: 'error');
-    expectLater(sut.emailErrorStream, emits('error'));
+
+    // Esperado o erro apenas uma vez aconteça
+    sut.emailErrorStream
+        .listen(expectAsync1((error) => expect(error, 'error')));
+
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
 
     sut.validateEmail(email);
+    sut.validateEmail(email);
+  });
+
+  test('Should emit null if validation succeds', () {
+    // Esperado o erro apenas uma vez aconteça
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, null)));
+
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validateEmail(email);
+    sut.validateEmail(email);
+  });
+
+  test('Should call validate with correct password', () {
+    sut.validatePassword(password);
+    verify(validation.validate(field: 'password', value: password)).called(1);
+  });
+
+  test('Should emit password error if validation fails', () {
+    mockValidation(value: 'error');
+
+    // Esperado o erro apenas uma vez aconteça
+    sut.passwordErroStream
+        .listen(expectAsync1((error) => expect(error, 'error')));
+
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validatePassword(password);
+    sut.validatePassword(password);
+  });
+
+  test('Should emit null if password succeds', () {
+    // Esperado o erro apenas uma vez aconteça
+    sut.passwordErroStream.listen(expectAsync1((error) => expect(error, null)));
+
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validatePassword(password);
+    sut.validatePassword(password);
+  });
+
+  test('Should emit password is email null validation', () {
+    mockValidation(field: 'email', value: 'error');
+
+    // Esperado o erro apenas uma vez aconteça
+    sut.emailErrorStream
+        .listen(expectAsync1((error) => expect(error, 'error')));
+    sut.passwordErroStream.listen(expectAsync1((error) => expect(error, null)));
+
+    sut.isFormValidStream
+        .listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+  });
+
+  test('Should emit password is email null validation', () async {
+    // Esperado o erro apenas uma vez aconteça
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, null)));
+    sut.passwordErroStream.listen(expectAsync1((error) => expect(error, null)));
+
+    //Quando preecher meu campo email incialmente
+    //meu campo esta invalido e depois preecher passowrd meu form ficar valido
+    expect(sut.isFormValidStream, emitsInOrder([false, true]));
+
+    sut.validateEmail(email);
+    //Esperar o delay para que o teste seja executado para dar true nos
+    //dois email e password
+    await Future.delayed(Duration.zero);
+    sut.validatePassword(password);
   });
 }
